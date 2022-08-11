3ef60994ee7b9d3a59b7545944b970c9640f9344
yxxjxxxhxxx <whdgus3012@gmail.com>
2022-08-12 00:02:51 +0900

    2.data_types

---
+++ 2.data_types/data_types.js
@@ -0,0 +1,20 @@
+let year = 2022;
+console.log(year);
+
+let grade = 4.5;
+console.log(grade);
+
+let greeting = '안녕하세요';
+
+let userName = 'Yjh';
+
+console.log('저는' + userName + '입니다'); //문자열 한에서 사용 가능
+console.log(1==1);
+console.log(``); //backtick 기호, 템플릿 리터럴 문법 `` 물결표
+console.log(`저는 ${userName} 입니다.`);
+
+let abokado = 'abokado';
+const mart = abokado;
+
+
+
